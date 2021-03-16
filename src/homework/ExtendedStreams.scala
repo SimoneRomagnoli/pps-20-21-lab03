858c41cd4771dca112f7a314124e8b5a60924514
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2021-03-16 13:02:25 +0100

    implementation, testing and refactoring of infinite sequence of fibonacci

--- src/homework/ExtendedStreams.scala
+++ src/homework/ExtendedStreams.scala
@@ -15,4 +15,14 @@ object ExtendedStreams {
 
   def constant[A](init: A): Stream[A] = Stream.iterate(init)(elem=>elem)
 
+  def fib(n:Int):Int = {
+    @annotation.tailrec
+    def _fib(n:Int, first:Int, second:Int): Int = n match {
+      case 0 => first
+      case 1 => second
+      case _ => _fib(n-1, second, first+second)
+    }
+    _fib(n, 0, 1)
+  }
+
 }
--- test/homework/TestExtendedStreams.scala
+++ test/homework/TestExtendedStreams.scala
@@ -1,5 +1,6 @@
 package homework
 
+import homework.ExtendedStreams._
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import u03.Lists.List.Cons
@@ -10,6 +11,7 @@ class TestExtendedStreams {
 
   val s = Stream.take(Stream.iterate(0)(_+1))(10)
   val x = "x"
+  val fibs:Stream[Int] = Stream.map(Stream.iterate(0)(_+1))(fib)
 
   @Test
   def dropTest: Unit = {
@@ -21,6 +23,12 @@ class TestExtendedStreams {
   def constantTest:Unit = {
     assertEquals(Cons(x, Cons(x, Cons(x, Cons(x, Cons(x, Nil()))))),
       Stream.toList(Stream.take(ExtendedStreams.constant(x))(5)))
+  }
+
+  @Test
+  def fibonacciTest:Unit = {
+    assertEquals(Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Cons(5, Cons(8, Cons(13, Nil())))))))),
+      Stream.toList(Stream.take(fibs)(8)))
   }
 
 }
